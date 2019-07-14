1057786c054e06911008bac2b6dd226dc8c905b0
Joakim Stien <joakim.stien@gmail.com>
2019-07-15 01:41:38 +0200

    Through some proper tomfoolery, I was able to update the weekly distance goal! :)

--- runtracker/runtracker/views/Goal/GoalDefineView.swift
+++ runtracker/runtracker/views/Goal/GoalDefineView.swift
@@ -1,17 +1,46 @@
 import SwiftUI
+import Combine
+
+private class DistanceGoalProxy: BindableObject {
+    var didChange = PassthroughSubject<Void, Never>()
+
+    var goalRepository: WeeklyGoalRepository?
+
+    var value: Double = 0 {
+        didSet {
+            let currentGoal = Int(goalRepository?.weeklyDistanceGoal?.distanceInMeters ?? 0)
+            let newGoal = Int(value) * 1000
+            if let goalRepository = goalRepository, currentGoal != newGoal {
+                goalRepository.weeklyDistanceGoal = WeeklyDistanceGoal(distanceInMeters: Double(newGoal))
+                didChange.send()
+            }
+        }
+    }
+}
 
 struct GoalDefineView : View {
-    @State var distance: Double = 30
+    private static let defaultGoal = 10.0
+
+    @EnvironmentObject var goalRepository: WeeklyGoalRepository
+    @ObjectBinding private var distanceProxy = DistanceGoalProxy()
 
     var body: some View {
-        VStack {
+        distanceProxy.goalRepository = goalRepository
+        return VStack {
             Panel(title: "Weekly Distance") {
                 VStack {
-                    Slider(value: $distance, from: 0, through: 100, by: 1)
+                    if goalRepository.weeklyDistanceGoal == nil {
+                        Text("You haven't set a weekly distance goal")
+                        Button("Set a Goal", action: {
+                            self.distanceProxy.value = GoalDefineView.defaultGoal
+                        })
+                    } else {
+                        Slider(value: $distanceProxy.value, from: 0, through: 100, by: 1)
 
-                    HStack {
-                        Text("Value: \(Int($distance.value))")
-                        Spacer()
+                        HStack {
+                            Text("Value: \(Int($distanceProxy.value.value))")
+                            Spacer()
+                        }
                     }
                 }
             }
@@ -23,7 +52,8 @@ struct GoalDefineView : View {
 #if DEBUG
 struct GoalDefineView_Previews : PreviewProvider {
     static var previews: some View {
-        GoalDefineView()
+        return GoalDefineView()
+            .environmentObject(WeeklyGoalRepository())
     }
 }
 #endif
