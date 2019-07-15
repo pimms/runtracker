c91ecc51a711a47133cbdb3717182c5cc629d5ab
Joakim Stien <joakim.stien@gmail.com>
2019-07-15 16:34:41 +0200

    Added persistence (UserDefaults) to WeeklyGoalRepository

--- runtracker/runtracker/core/model/goal/WeeklyGoalRepository.swift
+++ runtracker/runtracker/core/model/goal/WeeklyGoalRepository.swift
@@ -5,6 +5,37 @@ import Combine
 class WeeklyGoalRepository: BindableObject {
     var didChange = PassthroughSubject<Void, Never>()
 
-    var weeklyDistanceGoal: WeeklyDistanceGoal? { didSet { didChange.send() } }
-    var weeklyTimeGoal: WeeklyTimeGoal? { didSet { didChange.send() } }
+    var weeklyDistanceGoal: WeeklyDistanceGoal? {
+        didSet {
+            userDefaultsDistance = weeklyDistanceGoal?.distanceInMeters ?? -1
+            didChange.send()
+        }
+    }
+
+    var weeklyTimeGoal: WeeklyTimeGoal? {
+        didSet {
+            userDefaultsTime = weeklyTimeGoal?.durationInMinutes ?? -1
+            didChange.send()
+        }
+    }
+
+    // MARK: - Private properties
+
+    @UserDefault("weekly_distance_goal", defaultValue: -1)
+    private var userDefaultsDistance: Double
+
+    @UserDefault("weekly_time_goal", defaultValue: -1)
+    private var userDefaultsTime: Double
+
+    // MARK: - Methods
+
+    init() {
+        if userDefaultsDistance >= 0 {
+            weeklyDistanceGoal = WeeklyDistanceGoal(distanceInMeters: userDefaultsDistance)
+        }
+
+        if userDefaultsTime >= 0 {
+            weeklyTimeGoal = WeeklyTimeGoal(durationInMinutes: userDefaultsTime)
+        }
+    }
 }
